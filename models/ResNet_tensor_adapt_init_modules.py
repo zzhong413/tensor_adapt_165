d52aa5223092bc3b7259e70948a862c12833013c
zzhong413 <zzhong@caltech.edu>
2022-03-11 11:01:08 -0800

    update

--- models/ResNet_tensor_adapt_init_modules.py
+++ models/ResNet_tensor_adapt_init_modules.py
@@ -28,9 +28,9 @@ class AdaptConv(nn.Module):
 
     def forward(self, x, adapt=False):
         if not adapt:
-            l2_norm = torch.linalg.vector_norm(self.adaptive_weights_preconv)
+            # l2_norm = torch.linalg.vector_norm(self.adaptive_weights_preconv)
             adaptive_weights = self.adaptive_weights_preconv.repeat(x.shape[0], 1)
-            adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
+            # adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
             x = self.conv1(x, adaptive_weights)
         else:
             adaptive_weights = self.adaptive_weights_preconv.repeat(x.shape[0], 1)
@@ -62,9 +62,9 @@ class BasicBlock(nn.Module):
         residual = self.bn1(residual)
         residual = self.relu1(residual)
         if not adapt:
-            l2_norm = torch.linalg.vector_norm(self.adaptive_weights_conv1)
+            # l2_norm = torch.linalg.vector_norm(self.adaptive_weights_conv1)
             adaptive_weights = self.adaptive_weights_conv1.repeat(batch_size, 1)
-            adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
+            # adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
             residual = self.conv1(residual, adaptive_weights)
         else:
             adaptive_weights = self.adaptive_weights_conv1.repeat(batch_size, 1)
@@ -73,9 +73,9 @@ class BasicBlock(nn.Module):
         residual = self.bn2(residual)
         residual = self.relu2(residual)
         if not adapt:
-            l2_norm = torch.linalg.vector_norm(self.adaptive_weights_conv2)
+            # l2_norm = torch.linalg.vector_norm(self.adaptive_weights_conv2)
             adaptive_weights = self.adaptive_weights_conv2.repeat(batch_size, 1)
-            adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
+            # adaptive_weights += torch.randn_like(adaptive_weights) * l2_norm * 0.1
             residual = self.conv2(residual, adaptive_weights)
         else:
             adaptive_weights = self.adaptive_weights_conv2.repeat(batch_size, 1)
